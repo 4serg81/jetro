21b11fb4436419a2626cdae1ce921a3402306e96
4serg81 <sergchs81@yandex.ru>
2020-07-04 21:51:37 +0300

    add project

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+  $('.slider__main').slick({
+    arrows: true,
+    speed: 700,
+    asNavFor: '.slider__nav',
+    slidesToShow: 1,
+  });
+
+  $('.slider__nav').slick({
+    asNavFor: '.slider__main',
+    focusOnSelect: true,
+    slidesToShow: 6,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ],
+  });
+   
+ });
+
+ $('.header__menu-btn').on('click', function(){
+  $('.menu > ul').slideToggle();
+});
+
+$('.dropdown > li').on('click', function(){
+  $('dropdown__list').slideToggle();
+});
