9f776f21a5992fab66c3195e5fe23826d03d79d2
4serg81 <sergchs81@yandex.ru>
2020-07-06 00:14:06 +0300

    fix format

--- js/main.js
+++ js/main.js
@@ -27,10 +27,9 @@ $(document).ready(function(){
       },
     ],
   });
-   
- });
+});
 
- $('.header__menu-btn').on('click', function(){
+$('.header__menu-btn').on('click', function(){
   $('.menu > ul').slideToggle();
 });
 
